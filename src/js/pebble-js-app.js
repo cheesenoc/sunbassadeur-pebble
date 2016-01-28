7151b4cce6dffc4f981adab33eb8efafea990fdb
cheesenoc <cheesenoc@gmail.com>
2016-01-28 23:48:14 +0100

    initial

---
+++ src/js/pebble-js-app.js
@@ -0,0 +1,78 @@
+function iconFromWeatherId(weatherId) {
+  if (weatherId < 5) {
+    return 3;
+  } else if (weatherId < 7) {
+    return 2;
+  } else if (weatherId > 9) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
+ 
+function fetchWeather(latitude, longitude) {
+  var req = new XMLHttpRequest();
+  req.open('GET', 'http://data.netcetera.com:80/smn/smn/BER'); 
+ //   'lat=' + latitude + '&lon=' + longitude + '&cnt=1&appid=' + myAPIKey, true);
+  req.onload = function () {
+    if (req.readyState === 4) {
+      if (req.status === 200) {
+        console.log(req.responseText);
+        var response = JSON.parse(req.responseText);
+        var temperature = response.temperature;
+        var icon = iconFromWeatherId(response.sunshine);
+        var city = response.station.name;
+        console.log(temperature);
+        console.log(icon);
+        console.log(city);
+        Pebble.sendAppMessage({
+          'WEATHER_ICON_KEY': icon,
+          'WEATHER_TEMPERATURE_KEY': temperature + '\xB0C',
+          'WEATHER_CITY_KEY': city
+        });
+      } else {
+        console.log('Error');
+      }
+    }
+  };
+  req.send(null);
+}
+
+function locationSuccess(pos) {
+  var coordinates = pos.coords;
+  fetchWeather(coordinates.latitude, coordinates.longitude);
+}
+
+function locationError(err) {
+  console.warn('location error (' + err.code + '): ' + err.message);
+  Pebble.sendAppMessage({
+    'WEATHER_CITY_KEY': 'Loc Unavailable',
+    'WEATHER_TEMPERATURE_KEY': 'N/A'
+  });
+}
+
+var locationOptions = {
+  'timeout': 15000,
+  'maximumAge': 60000
+};
+
+Pebble.addEventListener('ready', function (e) {
+  console.log('connect!' + e.ready);
+  window.navigator.geolocation.getCurrentPosition(locationSuccess, locationError,
+    locationOptions);
+  console.log(e.type);
+});
+
+Pebble.addEventListener('appmessage', function (e) {
+  window.navigator.geolocation.getCurrentPosition(locationSuccess, locationError,
+    locationOptions);
+  console.log(e.type);
+  console.log(e.payload.temperature);
+  console.log('message!');
+});
+
+Pebble.addEventListener('webviewclosed', function (e) {
+  console.log('webview closed');
+  console.log(e.type);
+  console.log(e.response);
+});
